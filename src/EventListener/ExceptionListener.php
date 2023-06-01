e819154e5fb920d493f969bd3db6e35e0be5962e
dev <comptegitmada@gmail.com>
2023-06-01 14:29:46 +0300

    Correction accès

---
+++ src/EventListener/ExceptionListener.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\EventListener;
+
+use Twig\Environment;
+use App\Repository\CategoryRepository;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
+
+class ExceptionListener implements EventSubscriberInterface
+{
+    private $categoryRepository;
+    private $twig;
+
+    public function __construct(Environment $twig, CategoryRepository $categoryRepository){
+        $this->categoryRepository = $categoryRepository;
+        $this->twig = $twig;
+    }
+    
+    /**
+     * @param ExceptionEvent $event
+     * @return void
+     */
+    public function onKernelException(ExceptionEvent $event): void
+    {
+        $exception = $event->getThrowable();
+        
+
+        $categories = $this->categoryRepository->findAll();
+
+        $statusCode = $exception->getStatusCode();
+        $content = $this->twig->render('error/' . $statusCode . '.html.twig', [
+            'categories' => $categories,
+            'class' => '',
+            'active' => '',
+            'class_wrapper' => ''
+        ]);
+
+        $response = new Response();
+        $response->setContent($content);
+
+        $event->setResponse($response);
+    }
+
+    /**
+     * @return string[]
+     */
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            KernelEvents::EXCEPTION => 'onKernelException',
+        ];
+    }
+
+}
